37828b027a2139ddbde3fddd349addfeffc4762c
Shihabudheen US <shihabudheen@dunzo.in>
2021-04-18 08:15:49 +0530

    new

--- Easy/2.twoNumberSum.js
+++ Easy/2.twoNumberSum.js
@@ -1,3 +1,13 @@
+/**
+Write a function that takes in a non-empty array of distinct integers and an integer 
+representing a target sum. If any two numbers in the input array sum up to the target 
+sum, the function should return them in an array, in any order. If no two numbers sum 
+up to the target sum, the function should return an empty array.
+Note that the target sum has to be obtained by summing two different integers in 
+the array; you can't add a single integer to itself in order to obtain the target sum.
+You can assume that there will be at most one pair of numbers summing up to the target sum.
+*/
+
 // find a sum pair with a given target
 const arr = [3, 5, -4, 8, 11, -1, 6];
 const targetSum = 4;
@@ -56,7 +66,7 @@ function twoNumberSum(arr, targetSum) {
     const sum = arr[L] + arr[R];
     if (sum === targetSum) return [arr[L], arr[R]];
     else if (sum < targetSum) L++;
-    else if (sum > targetSum)  R--;
+    else if (sum > targetSum) R--;
   }
   return [];
 }
--- dry.js
+++ dry.js
@@ -1,29 +1,6 @@
-const array = [2, 1, 2, 2, 2, 3, 4, 2];
-const toMove = 2;
-
-function moveElementToEnd(array, toMove) {
-  let L = 0;
-  let R = array.length - 1;
-  let temp = null;
-
-  while (L < R) {
-    while (L < R && array[R] === toMove) {
-      R--;
-    }
-
-    while (L < R && array[L] !== toMove) {
-      L++;
-    }
-
-    temp = array[L];
-      array[L] = array[R];
-      array[R] = temp;
-
-    R--;
-    L++;
-  }
-
-  return array;
-}
-
-moveElementToEnd(array, toMove);
+const array=[
+  [1, 2, 3, 4],
+  [12, 13, 14, 5],
+  [11, 16, 15, 6],
+  [10, 9, 8, 7]
+]
